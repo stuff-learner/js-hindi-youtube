93151fe70c44027e83b72f670ea4614eae1ee334
stuff-learner <imamn.info@gmail.com>
2024-11-28 19:04:20 +0000

    02_dataTypes done!!

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // not allowed we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("aman kumar chaursiya")
+
+let name = "aman kumar chaursiya" // string dataType
+let age = 3 // number dataType
+let isLoggedIn = false // boolean dataType
+
+/*
+Primitive DataTypes:
+1. number => 2 to the power of 53
+2. bigint
+3. string => ""
+4. boolean => true/false
+5. null => standalone value
+6. udefined
+7. symbol => for uniqueness
+*/
+
+// object
+console.log(typeof "aman kumar chaursiya") // output: string
+console.log(typeof age); // output: number
+console.log(typeof null); // output: object
+console.log(typeof undefined); // output: undefined
