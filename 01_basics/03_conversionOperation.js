9876d8c15f310276cc550f1efe3cea5c548abb65
stuff-learner <imamn.info@gmail.com>
2024-11-30 20:10:50 +0000

    03_conversionOperation done!!

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -38,3 +38,43 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//--------------------OPERATIONS--------------------
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " aman kumar chaursiya"
+let str3 = str1 + str2
+console.log(str3);
+
+// aise code nhi likhne hote => yeh code likhne ka sahi tareeka nhi hai
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((3+4)*5%3);
+
+console.log(true); // output: true
+console.log(+true); // output: 1, yeh code likhne ka sahi tareeka nhi hai
+// console.log(true+); // error
+console.log(+""); // output: 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study: https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion 
